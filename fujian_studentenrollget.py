e7d0f0d956cee6dd5d12230f7081e8f88f74a185
Jira Aphisitworachorch <thaixperiasp@gmail.com>
2019-08-05 00:18:29 +0700

    V1.1 : Can get Student Name from ID

--- fujian_studentenrollget.py
+++ fujian_studentenrollget.py
@@ -2,18 +2,41 @@
 #!Python
 
 import re
+import requests
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 info = []
 
 def fetchall(studentid):
-    require_page = 'http://reg5.sut.ac.th/registrar/learn_time.asp?studentid=1' + studentid + '&f_cmd=2'
+    require_page = 'https://reg.sut.ac.th/registrar/learn_time.asp?studentid=1' + studentid + '&f_cmd=2'
     return require_page
 
 def urlreturn(stdid):
-    require_page = 'http://reg5.sut.ac.th/registrar/learn_time.asp?studentid=1' + stdid + '&f_cmd=2'
+    require_page = 'https://reg.sut.ac.th/registrar/learn_time.asp?studentid=1' + stdid + '&f_cmd=2'
     return require_page
+
+def getstudent_name(stdid):
+    r = requests.post("https://reg.sut.ac.th/registrar/learn_time.asp", data=dict(
+        f_cmd="1",
+        f_studentcode=stdid,
+        f_studentname="",
+        f_studentsurname="",
+        f_studentstatus="all",
+        departmentid="",
+        programid="",
+        f_maxrows="25"
+    ))
+    r.encoding = r.apparent_encoding
+    sr = BeautifulSoup(r.text, 'html.parser')
+    
+    namet = []
+    for mmp in sr.find_all('font', attrs={'face': 'MS Sans Serif', 'size': '3'}):
+        namet.append(mmp.find("font").text)
+
+    mrx = re.sub('<.*?>', '', str(namet[0])).replace('\xa0', '')
+    xm = mrx.strip("[]")
+    return xm
 
 def getstudentenrollment(url):
     page = urlopen(url)
